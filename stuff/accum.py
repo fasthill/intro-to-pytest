eb2cebf9cff20cf70372385e30c0a51d889c8162
fasthill <kangeun@naver.com>
2024-05-08 13:09:29 +0900

    nothing

--- stuff/accum.py
+++ stuff/accum.py
@@ -3,19 +3,19 @@ This module contains a basic accumulator class.
 Its purpose is to show how to use the pytest framework for testing classes.
 """
 
-#testtttttttttttt
+
 # --------------------------------------------------------------------------------
 # Class: Accumulator
 # --------------------------------------------------------------------------------
 
 class Accumulator:
 
-  def __init__(self):
-    self._count = 0
+    def __init__(self):
+        self._count = 0
 
-  @property
-  def count(self):
-    return self._count
-  
-  def add(self, more=1):
-    self._count += more
+    @property
+    def count(self):
+        return self._count
+
+    def add(self, more=1):
+        self._count += more
